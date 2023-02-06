d943df5d081b3ce80252a5f4ed9ea33cf42c2605
jasonkencana1 <119559579+jasonkencana1@users.noreply.github.com>
2023-02-05 22:48:51 -0700

    Add files via upload

---
+++ Lab1D/Person.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1D
+{
+    internal class Person
+    {
+
+        private int personId;
+        private string firstName;
+        private string lastName;
+        private string favoriteColour;
+        private int age;
+        private bool isWorking;
+
+
+        //methods
+       /* public void DisplayPersonInfo()
+        {
+            Console.WriteLine(firstName+ " " + lastName);
+            Console.WriteLine(this.firstName+ "'s favorite colour is " + this.favoriteColour);
+        }*/
+
+        public void DisplayList()
+        {
+            List<Object> newlist = new List<Object>();
+            newlist.Add(this.personId);
+            newlist.Add(this.firstName);
+            newlist.Add(this.lastName);
+            newlist.Add(this.favoriteColour);
+            newlist.Add(this.age);
+            newlist.Add(this.isWorking);
+            foreach (object item in newlist)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+
+        public string ChangeFavouriteColor()
+        {
+            this.favoriteColour = "White";
+            return this.favoriteColour;
+        }
+
+        public int GetAgeTenYears() 
+        { 
+            int newAge = age+10;
+
+            return newAge;
+        }
+
+        public override string ToString()
+        {
+            string formatted = "";
+
+            formatted += "Person ID : " + personId + "\n";
+            formatted += "First Name : " + firstName + "\n";
+            formatted += "Last Name : " + lastName + "\n";
+            formatted += "Favourite Color : " + favoriteColour + "\n";
+            formatted += "Age : " + age + "\n";
+            formatted += "Is Working : " + isWorking;
+            return formatted;
+        }
+
+
+        //properties
+        public int Id
+        {
+            get
+            {
+                return this.personId;
+            }
+        }
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+        }
+        public string FavoriteColour 
+        { 
+            get
+            {
+                return this.favoriteColour;
+            }
+        }
+        public int Age 
+        { 
+            get
+            {
+                return this.age;
+            }
+        }
+        public bool IsWorking 
+        { 
+            get 
+            {
+                return this.isWorking;
+            }
+        }
+
+        public Person(int personId, string firstName, string lastName, string favoriteColour, int age, bool isWorking)
+        {
+            this.personId = personId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.favoriteColour = favoriteColour;
+            this.age = age;
+            this.isWorking = isWorking;
+        }
+
+
+
+    }
+}
---
+++ Lab1D/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(1, "Ian", "Brooks", "Red", 30, true);
+            Person person2 = new Person(2, "Gina", "James", "Green", 18, false);
+            Person person3 = new Person(3, "Mike", "Briscoe", "Blue", 45, true);
+            Person person4 = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+
+            Console.WriteLine(person2.Id + ": " + person2.FirstName + " " + person2.LastName + "'s favorite colour is " + person2.FavoriteColour);
+            person3.DisplayList();
+            Console.WriteLine(person1.Id + ": " + person1.FirstName + " " + person1.LastName + "'s favorite colour is: " + person1.ChangeFavouriteColor());
+            Console.WriteLine(person4.FirstName + " " + person4.LastName + "'s Age in 10 years is: " + person4.GetAgeTenYears());
+
+            List<Person> PersonObject = new List<Person>();
+            PersonObject.Add(person1);
+            PersonObject.Add(person2);
+            PersonObject.Add(person3);
+            PersonObject.Add(person4);
+
+            int total = 0;
+
+            foreach (Person person in PersonObject)
+            {
+                total += person.Age;
+            }
+
+            double average = (double)total / 4;
+            Console.WriteLine("Average age is: " + average);
+
+
+            //create a variable (youngestPerson) that
+            //consists the first index of the object, if the person age on the list is less than
+            //youngestPerson than the youngestPerson will become the person on that list 
+
+            Person youngestPerson = PersonObject[0];
+            Person oldestPerson = PersonObject[0];
+            
+            foreach (Person person in PersonObject)
+            {
+                if(person.Age < youngestPerson.Age) 
+                { 
+                    youngestPerson = person;
+                }
+                if(person.Age > oldestPerson.Age)
+                {
+                    oldestPerson = person;
+                }
+            }
+
+            Console.WriteLine("the youngest person is: " + youngestPerson.FirstName);
+            Console.WriteLine("the oldest person is: " + oldestPerson.FirstName);
+
+            foreach (Person person in PersonObject)
+            {
+                if (person.FirstName.StartsWith("M"))
+                    {
+                        Console.WriteLine(person);
+                    }
+            }
+
+            foreach (Person person in PersonObject)
+            {
+                if (person.FavoriteColour == "Blue")
+                    {
+                        Console.WriteLine(person);
+                    }
+            }
+
+        }
+
+    }
+    
+}
